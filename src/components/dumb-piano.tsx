710e11492707ce9c6d323c265ef649822bd5963d
Michael Kochell <michael@iris-works.com>
2018-11-26 23:49:48 -0500

    remove react-piano

--- src/components/dumb-piano.tsx
+++ src/components/dumb-piano.tsx
@@ -1,8 +1,6 @@
 import React, {useContext} from 'react'
-import classnames from 'classnames'
 
-import { KeyboardShortcuts, MidiNumbers } from 'react-piano'
-import ReactPiano from '../react-piano/src/Piano';
+import { KeyboardShortcuts, MidiNumbers, Piano as ReactPiano } from 'react-piano'
 
 import 'react-piano/dist/styles.css';
 
